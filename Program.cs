7454d0213724ea83a70d03d28e1abb7afe1c2785
Rene Jock <rene.jock@outlook.de>
2022-12-26 19:14:08 +0100

    Added MainMenue Method
    added first fuctionality for the Main Menue.

--- Program.cs
+++ Program.cs
@@ -15,6 +15,42 @@ namespace TicTacToe
 
         static void MainMenue()
         {
+            bool end = false;
+            while (true)
+            {
+                Console.ForegroundColor= ConsoleColor.Blue;
+                Console.WriteLine("###################");
+                Console.WriteLine("### TIC TAC TOE ###");
+                Console.WriteLine("###################");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.WriteLine("Wähle eine Aktion:");
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("[1] Spiel starten");
+                Console.WriteLine("[2] Spiel Beenden");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.Write("Aktion: >>");
+                int menu = Convert.ToInt32(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        //StartGame();
+                        Console.WriteLine("Spiel wird gestartet...");
+                        break;
+                    case 2:
+                        end= true;
+                        break;
+
+
+                }
+                if (end)
+                {
+                    break;
+                }
+
+            }
 
         }
 
