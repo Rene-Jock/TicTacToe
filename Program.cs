1e2f533f6026089b9d5ca1f3229225e776b53a95
Rene Jock <rene.jock@outlook.de>
2022-12-26 18:57:22 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMenue();
+        }
+
+        static void MainMenue()
+        {
+
+        }
+
+        //static void StartGame()
+        //{
+
+        //}
+
+        //static void GameLoop()
+        //{
+           
+        //}
+    }
+}
